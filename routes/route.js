007c2a4284b4e94911033038b1c6ddc14c1cdb0d
sathishh2003 <sathishhh2003@gmail.com>
2024-08-01 21:39:06 +0530

    to upload netlfiy add some changes

--- routes/route.js
+++ routes/route.js
@@ -4,28 +4,28 @@ const path = require('path');
 
 
 router.get('/index',(req,res)=>{
-    res.sendFile(path.join(__dirname,'..','views','index.html'));
+    res.sendFile(path.join(__dirname,'..','.netlify','dist','index.html'));
 });
 router.get('/veg',(req,res)=>{
-    res.sendFile(path.join(__dirname,'..','views','veg.html'));
+    res.sendFile(path.join(__dirname,'..','.netlify','dist','veg.html'));
 });
 router.get('/nonveg',(req,res)=>{
-    res.sendFile(path.join(__dirname,'..','views','non_veg.html'));
+    res.sendFile(path.join(__dirname,'..','.netlify','dist','non_veg.html'));
 });
 router.get('/icecream',(req,res)=>{
-    res.sendFile(path.join(__dirname,'..','views','icecream.html'));
+    res.sendFile(path.join(__dirname,'..','.netlify','dist','icecream.html'));
 });
 router.get('/milkshakes',(req,res)=>{
-    res.sendFile(path.join(__dirname,'..','views','milkshakes.html'));
+    res.sendFile(path.join(__dirname,'..','.netlify','dist','milkshakes.html'));
 });
 router.get('/juices',(req,res)=>{
-    res.sendFile(path.join(__dirname,'..','views','juices.html'));
+    res.sendFile(path.join(__dirname,'..','.netlify','dist','juices.html'));
 });
 router.get('/cakes',(req,res)=>{
-    res.sendFile(path.join(__dirname,'..','views','cakes.html'));
+    res.sendFile(path.join(__dirname,'..','.netlify','dist','cakes.html'));
 });
 router.get('/contact',(req,res)=>{
-    res.sendFile(path.join(__dirname,'..','views','contact.html'));
+    res.sendFile(path.join(__dirname,'..','.netlify','dist','contact.html'));
 });
 
 
