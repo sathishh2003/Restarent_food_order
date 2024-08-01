089e693af5c8707f158ac60731a3a4212a73f2cc
sathishh2003 <sathishhh2003@gmail.com>
2024-08-01 23:08:29 +0530

    reset the project to old

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+/* const serverless = require('serverless-http'); */
+const PORT = 14452;
+const path = require('path');
+const router = require('./routes/route');
+
+app.use(express.static(path.join(__dirname,'views')));
+app.use('/',router);
+
+
+/* app.use('/.netlify/functions/app',app)
+module.exports.handler = serverless(app); 
+ */
+app.listen(PORT,()=>{
+    console.log(PORT+" is running now!");
+});
